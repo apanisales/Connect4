ef4f85e98a815e2fb3602b016bc235e6f92250dd
Anthony Panisales <anthony.jason14@att.net>
2022-01-02 19:58:30 -0800

    feature-37: Enhancing the look of the Select First Player Modal

--- src/components/CustomButton.js
+++ src/components/CustomButton.js
@@ -4,7 +4,7 @@ const CustomButton = styled.button`
     background-color: black;
     color: white;
     font-size: 1.2rem;
-    padding: 1rem 3rem;
+    padding: 1rem 2rem;
     border-radius: 5px;
     margin: 1rem 1rem;
     cursor: pointer;
@@ -13,6 +13,12 @@ const CustomButton = styled.button`
     &:hover {
       color: grey;
       cursor: pointer;
+    }
+
+    &:disabled {
+      color: grey;
+      opacity: 0.7;
+      cursor: default;
     }
   `;
 
